f7fc2f9189948a398f172768146e4bbe3f56c5db
Ryan <ravalles10@gmail.com>
2022-11-11 20:17:43 +0900

    Add game function and user input

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 //make computer choose from rock, paper or scissors
 function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissors'];
-    const random = choice[Math.floor(Math.random() * choice.length)];
- return random;
+    const choice = ['rock', 'paper', 'scissors'];
+    return choice[Math.floor(Math.random() * choice.length)];
+ 
 }
-
+let playerWins = 0
+let computerWins = 0
 //make player selection
-function playerSelection(){}
-
 //take selections and determine outcome
-
-const computerChoice = getComputerChoice()
-const playerChoice = playerSelection()
-
-function playRound(computerChoice, playerChoice){
+function playRound(){
+    const playerChoice = prompt('rock, paper or scissors').toLowerCase();
+    const computerChoice = getComputerChoice()
     if (computerChoice === playerChoice){
-        return 'tie'
+        computerWins += 0, playerWins += 0
     }
     else if ((playerChoice === 'rock' && computerChoice === 'scissors') || 
     (playerChoice === 'scissors' && computerChoice === 'paper') || 
     (playerChoice === 'paper' && computerChoice === 'rock')){
-        return 'You win';
+        playerWins += 1
+    }
+    else if (playerChoice == 'rock' || 'scissors' || 'paper'){
+        return 'Please enter rock, paper or scissors.'
     }
     else{
-        return 'Computer wins'
+        computerWins += 1
     }
 }
-//
-
+//create function called game which makes the round loop 5 times then the most wins is declared winner
+//make the game loop 5 times
+// 3 out of 5 wines equals game won
+function game(){
+    
+    for (let i = 0; i < 5; i++){
+        playRound();      
+}
+  if (playerWins === computerWins){
+    return "It's a draw!"
+  }
+  if (playerWins < computerWins){
+    return "Computer wins!"
+  }
+  if (playerWins > computerWins){
+    return "You win!"
+  }
+}
 
 
